10197428a5e8c549b6ef8dcb389e1db69276c82f
Alex Luu <alexluu@brandeis.edu>
2015-12-14 07:03:45 +0700

    First round of interface design done.

--- app_speech/forms.py
+++ app_speech/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class TranscriptForm(forms.Form):
     transcript = forms.CharField(label='', widget=\
-                                 forms.Textarea(attrs={'cols':100, 'rows':10}))
+                                 forms.Textarea(attrs={'cols':70, 'rows':10}))
 
 class UploadForm(forms.Form):
     file = forms.FileField(
