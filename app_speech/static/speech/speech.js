b41bac5ed74767cda9647666295147b6a74ff471
Alex Luu <alexluu@brandeis.edu>
2015-12-16 14:19:22 +0700

    Improved speech recognizing interface.

--- app_speech/static/speech/speech.js
+++ app_speech/static/speech/speech.js
@@ -1,32 +1,27 @@
 if (!('webkitSpeechRecognition' in window)) {
 	alert("Sorry, your Browser does not support the Speech API");
 } else {
-	var recognizing;
+	var recognizing = false;
 	var recognition = new webkitSpeechRecognition();
 	recognition.continuous = true;
 	recognition.interimResults = true;
 	recognition.lang = "en-US";
-	reset();
 	recognition.onend = reset;
+	var textarea = document.getElementById('speech');
+	final_span.innerHTML = textarea.value;
+	interim_span.innerHTML = "";
 
 	recognition.onresult = function (e) {
-		var textarea = document.getElementById('speech');
+		var interim = "";
 		for (var i = e.resultIndex; i < e.results.length; ++i) {
 			if (e.results[i].isFinal) {
-				textarea.value += e.results[i][0].transcript + ' /';			
+				textarea.value += e.results[i][0].transcript + ' / ';	
+			} else {
+				interim += e.results[i][0].transcript;
 			}
 		}
-		var final = "";
-		var interim = "";
-		for (var i = 0; i < event.results.length; ++i) {
-			if (e.results[i].isFinal) {
-				final += event.results[i][0].transcript + ' /';
-			} else {
-				interim += event.results[i][0].transcript;
-        }
-      }
-      final_span.innerHTML = final;
-      interim_span.innerHTML = interim;
+		final_span.innerHTML = textarea.value;
+		interim_span.innerHTML = interim;
 	}
 	
 	function reset() {
@@ -34,45 +29,33 @@ if (!('webkitSpeechRecognition' in window)) {
 		//sb1.innerHTML = "Click to Speak";
 		sb1.style.backgroundColor = "transparent";
 		start_img.alt = "Start";
-	}
-
-	function postText() {
-		var data = {'speech_text': document.getElementById('speech').value};
-		console .log (data)
-		$.post(URL, data, function(response){
-			// console .log (data)
-			if(response != 'success'){ alert('Error in getting speech!'); }
-		});			
-	}
+	}	
 
 	function toggleStartStop() {
+		var data = {'speech_text': textarea.value};
 		if (recognizing) {
 			recognition.stop();
 			reset();
-			var data = {'speech_text': document.getElementById('speech').value};
-			console .log (data)
 			$.post(URL, data, function(response){
-				// console .log (data)
+				//console.log(data);
 				if(response != 'success'){ alert('Error in getting speech!'); }
 			});			
-		} else {
-			// // get speech text
-			// var data = {'speech_text': document.getElementById('speech').value};
-			// console .log (data)
-			// $.post(URL, data, function(response){
-			// 	// console .log (data)
-			// 	if(response != 'success'){ alert('Error in getting speech!'); }
-			// });			
+		} else {		
 			recognition.start();
 			recognizing = true;
 			//sb1.innerHTML = "Click to Stop";
 			sb1.style.backgroundColor = "green";
 			start_img.alt = "Stop";
-			final_span.innerHTML = "";
-			interim_span.innerHTML = "";
 		}
 	}
 	
+	function postText() {
+		var data = {'speech_text': textarea.value};
+		$.post(URL, data, function(response){
+			//console.log(data)
+			if(response != 'success'){ alert('Error in getting speech!'); }
+		});			
+	}
 	
 }
 
